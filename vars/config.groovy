59e20c11abacbe9c81d0826c83a6e8672b14ad5d
Vladimir <vladimir-2@yandex.ru>
2020-04-29 14:35:59 +0300

    config map fix

--- vars/config.groovy
+++ vars/config.groovy
@@ -3,22 +3,26 @@
 
 def calculateConfig(environment) {
   configFileProvider([configFile(fileId: 'web_config', variable: 'CONFIG')]){
-    config = readJSON file: "${CONFIG}"
+    def conf_map = readJSON file: "${CONFIG}"
     try {
-      stack = stacks."${environment}"
+      stack = conf_map."${environment}"
       def instances = [authors: stack.authors, publishers: stack.publishers, dispatchers: stack.dispatchers]
       log.printMagenta("[INFO] Found stack for ${environment}")
       instances.authors.each {author ->
+        log.printMagenta("[INFO] Authors:")
         log.printMagenta("[INFO] ${author}")
       }
       instances.publishers.each {publisher ->
+        log.printMagenta("[INFO] Publishers:")
         log.printMagenta("[INFO] ${publisher}")
       }
       instances.dispatchers.each {dispatcher ->
+        log.printMagenta("[INFO] Dispatchers:")
         log.printMagenta("[INFO] ${dispatcher}")
       }
-      return instances
-      
+      return instances.authors
+      return insatnces.publishers
+      return instances.dispatchers      
     } catch(Exception ex) {
       log.printRed("[ERROR] Can't find provided environment")
       log.printRed("[ERROR] Check if ${environment} is in a config")
