ff49e5971c76970d326300a69dd3c4043e08fc38
Vladimir <vladimir-2@yandex.ru>
2020-05-08 14:54:26 +0300

    Invalidate cache fix

--- vars/aem.groovy
+++ vars/aem.groovy
@@ -42,7 +42,7 @@ def invalidateCache(configObject) {
   configObject.dispatchers.each {dispatcher ->
     log.printMagenta("[INFO] Invalidating cache on ${dispatcher}")
     sh(script: "curl -I -X GET --header 'CQ-Action:Delete' --header 'CQ-Handle:/content' --header 'CQ-Path:/content' --header 'CQ-Path:/etc' http://${dispatcher}/invalidate.cache")
-    sh(script: "curl -I -X POST --header 'CQ-Action:Activate' --header 'CQ-Handle:path-pattern' --header 'Content-Length:0' http://${dispatcher}/invalidate.cache") }
+    sh(script: "curl -H 'CQ-Action:Delete' -H 'CQ-Handle:/etc/designs/orion' -H 'CQ-Path:/etc/designs/orion' -H 'Content-Length:0' -H 'Content-Type:application/octet-stream' --noproxy .com http://${dispatcher}/invalidate.cache")
 }
 
 def flushJsp(configObject) {
