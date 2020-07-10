a5dc740214437cb85037b84f25add5c48b5ff1ad
Vladimir <vladimir-2@yandex.ru>
2020-07-10 16:54:13 +0300

    Invalidate JSP

--- vars/aem.groovy
+++ vars/aem.groovy
@@ -226,6 +226,7 @@ def flushJsp(configObject) {
     instances.each {instance ->
       log.printMagenta("[INFO] Sending cURL to slingjsp on ${instance}")
       sh(script: "curl -u ${admin} -I -X POST http://${instance}/system/console/slingjsp")
+      sh(script: "curl -u ${admin} -X POST http://${instance}/system/console/scriptcache")
     }
   }
 }
