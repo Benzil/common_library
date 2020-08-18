bad124ae453844b216eb5bf83271a19dddcab008
Vladimir <vladimir-2@yandex.ru>
2020-08-18 16:42:20 +0300

    fix

--- vars/aem.groovy
+++ vars/aem.groovy
@@ -256,7 +256,7 @@ def clearJspCache65(configObject) {
   instances.each {instance ->
     def bundle_path = sh(script: "ssh -o StrictHostKeyChecking=no aem@${instance[0..-6]} 'grep -rn org.apache.sling.commons.fsclassloader /opt/aem/*/crx-quickstart/launchpad/felix/*'")
     def pattern = /^(.+).bundle.info/
-    def result = bundle_path =~ pattern
+    def result = bundle_path[0] =~ pattern
 
     log.printMagenta("Found path ${result[0][1]}")
     
